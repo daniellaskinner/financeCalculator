42da2fd8debbb9efb1fc6d1c208c454b6fcbe954
daniellaskinner <daniellaskinner120@gmail.com>
2020-05-10 15:14:38 +0100

    changed colour theme and centered content

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 
 document.querySelector("button").addEventListener("click", ()=> {
     //WHY CAN THESE NOT BE DECLARED GLOBALLY?
-    let adminFee = getAdminFee(getInputBorrowAmount())
-    let borrowAmount = getInputBorrowAmount()
-    let totalLoanCost = calculateRepaymentAmount(borrowAmount)
-    let monthlyRepaymentAmount = calculateMonthlyRepaymentAmount(getInputMonthlyRepaymentPercentage(), getCustomerSalary())
-    let loanDuration = calculateLoanDuration(totalLoanCost, monthlyRepaymentAmount)
+    let adminFee = getAdminFee(getInputBorrowAmount());
+    let borrowAmount = getInputBorrowAmount();
+    let totalLoanCost = calculateRepaymentAmount(borrowAmount);
+    let monthlyRepaymentAmount = calculateMonthlyRepaymentAmount(getInputMonthlyRepaymentPercentage(), getCustomerSalary());
+    let loanDuration = calculateLoanDuration(totalLoanCost, monthlyRepaymentAmount);
 
     if(borrowAmount <= 8000 && borrowAmount > 0){
-        document.getElementById("adminFee").textContent = adminFee
-        document.getElementById("loan").textContent = totalLoanCost
-        document.getElementById("monthlyRepaymentCost").textContent = monthlyRepaymentAmount
+        document.getElementById("adminFee").textContent = adminFee;
+        document.getElementById("loan").textContent = totalLoanCost;
+        document.getElementById("monthlyRepaymentCost").textContent = monthlyRepaymentAmount;
         document.getElementById("loanDuration").textContent = loanDuration
     } else {
         document.getElementById("result").innerHTML = "Cannot calculate loan. You may only borrow between £0.01-£8000"
     }
-})
+});
 
 //VALIDATE FOR NEGATIVE SALARY AND NEGATIVE REPAYMENT PERCENTAGE
